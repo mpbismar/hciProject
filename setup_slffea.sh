d6c3b04df9bd970edc42f72f7981a3b52a65bbb4
Julian Müller <julian.mueller@student.hpi.de>
2015-10-29 04:00:41 +0100

    Setupscript

---
+++ setup_slffea.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt-get install libgl1-mesa-dev
+sudo apt-get install libglu1-mesa-dev 
+sudo apt-get install freeglut3-dev
+sudo apt-get install libxmu-headers libxmuu1 libxmuu1-dbg libxmuu-dev libxmu-dev libxmu6 libxmu6-dbg
+sudo apt-get install libxi6 libxi6-dbg libxi-dev
